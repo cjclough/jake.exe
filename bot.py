408a95efd1f677ff1c7706987e1cc246d5003a28
Jake Clough <cjacobclough@gmail.com>
2018-09-15 23:17:14 -0500

    small changes

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def sanitize(message):
     # remove emojis and mentions
     message = re.sub(r'<.*>', '', message)
     # remove special characters
-    message = re.sub('[^A-Za-z0-9 /\',.?\"]+', '', message)
+    message = re.sub('[^A-Za-z0-9 /\',.?\"-]+', '', message)
     # remove whitespace
     message = message.strip()
 
@@ -43,10 +43,10 @@ def is_command(message):
             return True
     return False
 
-async def type_message(channel):
+async def type_message(channel, filename):
     await asyncio.sleep(2)
     async with channel.typing():
-        message = do_markov("./config/history.txt")
+        message = do_markov(filename)
         await asyncio.sleep(random.randint(4,6))
 
     await channel.send(message)
@@ -58,58 +58,53 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    bot.user_id = 137723737922338816
+    bot.owner_id = 137723737922338816
     await bot.change_presence(activity=discord.Game("markov.exe"))
 
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
 
-    if "<@"+str(bot.user.id)+">" in message.content:
+    if str(bot.user.id) in message.content:
         channel = message.channel
-        await type_message(channel)
+        await type_message(channel, "./config/"+str(message.guild.id)+"-"+str(message.author.id)+"-history.txt")
 
         def check(m):
-            return (m.channel == channel) and ("<@"+str(bot.user.id)+">" not in m.content) and (m.author.id != bot.user.id)
+            return (m.channel == channel) and ("<@"+str(bot.user.id)+">" not in m.content) and (m.author.id != bot.user.id) and (m.author.id == author)
 
         while True:
             try:
+                author = message.author.id
                 await bot.wait_for('message', check = check, timeout=20)
             except asyncio.TimeoutError:
                 break
-            await type_message(channel)
-
-    if "jake" in message.content.lower():
-        channel = message.channel
-        await type_message(channel)
+            await type_message(channel, "./config/"+str(message.guild.id)+"-"+str(message.author.id)+"-history.txt")
 
     if isinstance(message.channel, discord.DMChannel) and (message.author.id != bot.user.id):
-        await type_message(message.channel)
-
-    if message.author.id == bot.owner_id and not is_command(message.content):
-       message = sanitize(message.content)
-       with open("./config/history.txt", "a") as _file:
-            _file.write(message + "\n")
+        await type_message(message.channel, "./config/"+str(message.guild.id)+"-"+str(message.author.id)+"-history.txt")
 
 
 # generate a sentence
 @bot.command()
-async def markov(ctx):
-    await type_message(ctx.channel)
+async def speak(ctx):
+    await type_message(ctx.channel, "./config/"+str(ctx.guild.id)+"-"+str(ctx.author.id)+"-history.txt")
+
+
 
 # get all messages from server
 @bot.command()
-@is_owner()
 async def load(ctx):
-    channels = bot.get_all_channels()
+    channels = ctx.guild.text_channels
     for channel in channels:
-        await ctx.channel.send("Collecting messages in #"+channel.name)
-        if not (channel.name == "spam"):
+        if not channel.is_nsfw():
+            await ctx.channel.send("Collecting messages in #"+channel.name)
             async for message in channel.history(limit=None, reverse=True):
-                if message.author.id == owner and not is_command(message.content):
+                if message.author.id == ctx.author.id and not is_command(message.content):
                     message = sanitize(message.content)
                     if len(message) > 0:
-                        with open("./config/history.txt", "a") as _file:
+                        server = ctx.guild.id
+                        user = ctx.author.id
+                        with open("./config/"+str(server)+"-"+str(user)+"-history.txt", "a") as _file:
                             _file.write(message + "\n")
     await ctx.send("Loading complete.")
 
@@ -118,8 +113,6 @@ async def load(ctx):
 @is_owner()
 async def q(ctx):
     await bot.logout()
-
-#bot.loop.create_task(random_message_loop())
 
 # run
 bot.run(token)
